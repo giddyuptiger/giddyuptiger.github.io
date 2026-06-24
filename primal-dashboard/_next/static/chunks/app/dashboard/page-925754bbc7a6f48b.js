7423b7651e27d8bfaea7b439da9a50a7e6ac28aa
Jeremy Irons <jeremydirons@gmail.com>
2026-06-24 18:17:38 +0000

    primal-dashboard: deploy authenticated reader over stale sample root (Supabase Auth + RLS); post-login lands on /dashboard

--- primal-dashboard/_next/static/chunks/app/dashboard/page-925754bbc7a6f48b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[702],{653:function(n,e,u){Promise.resolve().then(u.bind(u,4201))}},function(n){n.O(0,[464,201,115,364,744],function(){return n(n.s=653)}),_N_E=n.O()}]);
---
+++ primal-dashboard/_next/static/chunks/app/page-ea865f34422317c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{512:function(n,e,u){Promise.resolve().then(u.bind(u,927))}},function(n){n.O(0,[899,927,971,117,744],function(){return n(n.s=512)}),_N_E=n.O()}]);

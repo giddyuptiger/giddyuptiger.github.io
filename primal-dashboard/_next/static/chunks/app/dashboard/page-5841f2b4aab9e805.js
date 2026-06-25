d6aa296ba1d1b83caedae607d260198318174f59
giddyuptiger <jeremydirons@gmail.com>
2026-06-25 11:54:00 +0000

    primal-dashboard: rebuild with pre-migration 404-banner suppression + full contrast/mobile pass (supersedes prior instructor-view publish)

--- primal-dashboard/_next/static/chunks/app/dashboard/page-5841f2b4aab9e805.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[702],{5779:function(n,e,u){Promise.resolve().then(u.bind(u,2271))}},function(n){n.O(0,[699,271,971,117,744],function(){return n(n.s=5779)}),_N_E=n.O()}]);
--- primal-dashboard/_next/static/chunks/app/page-58a76ade8a0f4e8e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{9516:function(n,e,u){Promise.resolve().then(u.bind(u,2271))}},function(n){n.O(0,[699,271,971,117,744],function(){return n(n.s=9516)}),_N_E=n.O()}]);
